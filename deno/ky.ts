a6e25bd49bbfe871bd2affeb0bcba53e2783c2d5
Chase May <chaseloganmay@gmail.com>
2021-04-08 17:03:15 -0600

    Remove assignment

--- deno/ky.ts
+++ deno/ky.ts
@@ -1,6 +1,6 @@
 import ky from "https://cdn.skypack.dev/ky?dts";
 
-const json = await ky.get("https://jsonplaceholder.typicode.com/todos/1")
+await ky.get("https://jsonplaceholder.typicode.com/todos/1")
   .json()
   .then((json) => {
     console.log(json);
